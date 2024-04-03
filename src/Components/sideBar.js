1461a1b29f3cf579d8e0eae528f1aff6626e833d
Sidhant-gupta07 <sidhantguptabdk123@gmail.com>
2024-04-04 01:27:10 +0530

    first commit

---
+++ src/Components/sideBar.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import logo from '../Assets/logo.png'
+const sideBar = () => {
+  return (
+    <div>
+        <img src='../Assets/logo.png' alt='logo'/>
+    </div>
+  )
+}
+
+export default sideBar
+
